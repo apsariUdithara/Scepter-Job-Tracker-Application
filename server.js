df1e1b443e68e1e863044bab3053bd2cce2d0ac3
Udithara Fernando <fernando_sau_e23@engug.ruh.ac.lk>
2024-06-05 10:15:03 +0530

    Update and delete requests commit

--- server.js
+++ server.js
@@ -50,17 +50,46 @@ app.post("/api/v1/jobs", (req, res) => {
   res.status(201).json({ job });
 });
 
-
 //GET a single Job
 app.get("/api/v1/jobs/:id", (req, res) => {
-  const{id}=req.params;
-  const job=jobs.find((job)=>job.id===id);
-if(!job){
-    return res.status(404).json({msg:`no job with id ${id}`});
-}
-res.status(200).json({job});
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
 });
 
+//Update Job
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "job modified", job });
+});
+
+//DELETE Job
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+  res.status(200).json({ msg: "job deleted", job });
+});
 
 const port = process.env.PORT || 5100;
 
