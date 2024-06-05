b5ce718413cae440b322bacc550b45563718ad38
Udithara Fernando <fernando_sau_e23@engug.ruh.ac.lk>
2024-06-05 07:09:26 +0530

    POST request

--- server.js
+++ server.js
@@ -29,6 +29,28 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//Get All Jobs
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+//Create Job
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    res.status(400).json({ msg: "please provide company and position" });
+    return;
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
+});
+
+app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
 });
