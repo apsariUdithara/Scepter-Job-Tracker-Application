3833e6aeccaa76b0215f18294eb1c1ce95b68612
Udithara Fernando <fernando_sau_e23@engug.ruh.ac.lk>
2024-06-11 13:11:21 +0530

    102.Authenticate user setup

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ import authRouter from "./routes/authRouter.js";
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import {authenticateUser} from "./middleware/authMiddleware.js";
+
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -24,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser ,jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
