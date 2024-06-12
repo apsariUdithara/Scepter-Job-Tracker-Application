f76972a4a324f99727e67d21656fb2f487a96692
Udithara Fernando <fernando_sau_e23@engug.ruh.ac.lk>
2024-06-13 03:54:49 +0530

    109.Get current user without password property

--- models/UserModel.js
+++ models/UserModel.js
@@ -18,4 +18,10 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+UserSchema.methods.toJSON = function () {
+  let obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export default mongoose.model("User", UserSchema);
