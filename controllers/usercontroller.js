907849e465c25b1cdc7febe0d662568f9b918300
Udithara Fernando <fernando_sau_e23@engug.ruh.ac.lk>
2024-06-14 20:49:42 +0530

    111.Get Application Stats

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -9,7 +9,9 @@ export const getCurrentUser = async (req, res) => {
 };
 
 export const getapplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "application stats" });
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+  res.status(StatusCodes.OK).json({ users, jobs });
 };
 
 export const updateUser = async (req, res) => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,10 +5,14 @@ import {
   updateUser,
 } from "../controllers/usercontroller.js";
 import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
+import { authorizePermissions } from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.get("/current-user", getCurrentUser);
-router.get("/admin/app-stats", getapplicationStats);
-router.patch("/update-user", validateUpdateUserInput,updateUser);
+router.get("/admin/app-stats", [
+  authorizePermissions("admin"),
+  getapplicationStats,
+]);
+router.patch("/update-user", validateUpdateUserInput, updateUser);
 
 export default router;
