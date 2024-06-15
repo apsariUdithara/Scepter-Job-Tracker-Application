10905c23829e79ae89c1bcf885479a04131e63f4
Udithara Fernando <fernando_sau_e23@engug.ruh.ac.lk>
2024-06-15 09:54:48 +0530

    112.Proxy setup

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import "express-async-errors";
 import * as dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
 const app = express();
 import morgan from "morgan";
@@ -12,7 +11,6 @@ import cookieParser from "cookie-parser";
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
-
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -29,11 +27,13 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
-app.use("/api/v1/users", authenticateUser,userRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
-
-
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
