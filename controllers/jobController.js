e7ee18cc9154ceeb76d1b4bafa98f1babef45731
Apsari Udithara <udithara169@gmail.com>
2024-07-25 19:52:41 +0530

    157.Stats Route Setup

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,14 +1,14 @@
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
-
+import mongoose from "mongoose";
+import day from "dayjs";
 export const getAllJobs = async (req, res) => {
-  
-  const jobs = await Job.find({createdBy:req.user.userId});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  req.body.createdBy=req.user.userId
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
@@ -30,4 +30,28 @@ export const deleteJob = async (req, res) => {
   const removedJob = await Job.findByIdAndDelete(req.params.id);
 
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: removedJob });
+};
+
+export const showStats = async (req, res) => {
+  const defaultStats = {
+    pending: 22,
+    interview: 11,
+    declined: 4,
+  };
+  let monthlyApplications = [
+    {
+      date: "May 23",
+      count: 12,
+    },
+    {
+      date: "June 23",
+      count: 9,
+    },
+
+    {
+      date: "July 23",
+      count: 3,
+    },
+  ];
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -8,6 +8,7 @@ import {
   getJob,
   updateJob,
   deleteJob,
+  showStats,
 } from "../controllers/jobController.js";
 import {
   validateJobInput,
@@ -19,6 +20,8 @@ router
   .route("/")
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
+
+router.route("/stats").get(showStats);
 router
   .route("/:id")
   .get(validateIdParam, getJob)
