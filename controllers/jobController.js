1bf61737e5701ca678f5d28e21aaee7da08afd3b
Apsari Udithara <udithara169@gmail.com>
2024-07-31 11:19:33 +0530

    159.Group by mothly applications

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -52,20 +52,35 @@ export const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  let monthlyApplications = [
-    {
-      date: "May 23",
-      count: 12,
-    },
-    {
-      date: "June 23",
-      count: 9,
-    },
 
+  let monthlyApplications = await Job.aggregate([
     {
-      date: "July 23",
-      count: 3,
+      $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) },
     },
-  ];
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format("MMM YY");
+      return { date, count };
+    })
+    .reverse();
+
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
