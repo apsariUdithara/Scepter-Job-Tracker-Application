a86fe31d26288ce3f9a9401888ebe14d4812b24c
Apsari Udithara <udithara169@gmail.com>
2024-09-24 09:51:22 +0530

    176.Pagination Logic

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -11,11 +11,28 @@ const PageBtnContainer = () => {
   const pages = Array.from({ length: numOfPages }, (_, index) => {
     return index + 1;
   });
-  console.log(pages);
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  console.log(search, pathname);
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+    console.log(pageNumber);
+  };
 
   return (
     <Wrapper>
-      <button className="btn prev-btn">
+      <button
+        className="btn prev-btn"
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) prevPage = numOfPages;
+          handlePageChange(prevPage);
+        }}
+      >
         <HiChevronDoubleLeft />
         prev
       </button>
@@ -27,13 +44,21 @@ const PageBtnContainer = () => {
                 pageNumber === currentPage && "active"
               }`}
               key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
             >
               {pageNumber}
             </button>
           );
         })}
       </div>
-      <button className="btn next-btn">
+      <button
+        className="btn next-btn"
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > numOfPages) nextPage = 1;
+          handlePageChange(nextPage);
+        }}
+      >
         next
         <HiChevronDoubleRight />
       </button>
